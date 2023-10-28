3d51828a763da84ef5ababae18551601455cb105
Khondamir Kadirov <106467756+KhondamirK@users.noreply.github.com>
2023-10-28 14:31:45 +0100

    Add files via upload

---
+++ dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+VanillaTilt.init(document.querySelectorAll(".box"), {
+  max: 25,
+  speed: 400
+}); //It also supports NodeList
+
+VanillaTilt.init(document.querySelectorAll(".box"));
